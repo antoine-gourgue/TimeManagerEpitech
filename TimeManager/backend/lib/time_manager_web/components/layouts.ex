be4ef642556a1944a9ca61c17f416b1b21561e4f
Antoine <antoine.gourgue@epitech.eu>
2024-10-11 23:23:35 +0200

    feat: init projet and add docker

---
+++ TimeManager/backend/lib/time_manager_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule TimeManagerWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use TimeManagerWeb, :controller` and
+  `use TimeManagerWeb, :live_view`.
+  """
+  use TimeManagerWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ TimeManager/backend/lib/time_manager_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule TimeManagerWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use TimeManagerWeb, :html
+
+  embed_templates "page_html/*"
+end
