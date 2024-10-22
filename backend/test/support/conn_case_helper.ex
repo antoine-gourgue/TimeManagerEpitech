7967dbda681aa6bb5d7b0ef17728e14459545a3b
Antoine <antoine.gourgue@epitch.eu>
2024-10-22 18:55:05 +0200

    fix: test

---
+++ backend/test/support/conn_case_helper.ex
@@ -0,0 +1,18 @@
+defmodule TimeManagerWeb.ConnCaseHelper do
+  alias TimeManager.Accounts
+
+  def authenticate_conn(conn) do
+    # Création d'un utilisateur de test
+    {:ok, user} = Accounts.create_user(%{
+      username: "test_user",
+      email: "test@example.com",
+      password: "password"
+    })
+
+    # Génération du token JWT
+    {:ok, token, _claims} = Accounts.generate_jwt(user)
+
+    # Ajout de l'en-tête Authorization avec le token JWT
+    put_req_header(conn, "authorization", "Bearer #{token}")
+  end
+end
